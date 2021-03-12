0245b7b07e821a0de3a21c610418ab774fe79f7e
Shana Hu <shanawho@gmail.com>
2021-03-12 14:08:11 -0800

    local storage

--- code.js
+++ code.js
@@ -1,17 +1,48 @@
 // import _ from 'lodash';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowercase = "abcdefghijklmnopqrstuvwxyz";
 let numbers = "0123456789";
 // let symbols = ".,;:'\"~&*!@#$"
 let glyphSets = [capitals, lowercase, numbers];
-var components = {};
-figma.showUI(__html__);
+var currTypeface;
+var currComponents = {};
+// {name: Typeface}
+var typefacesByName = {};
+function getTypefacesFromStorage() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // What is necessary at startup
+        // List of names of typefaces
+        // All the Typeface class objects which contain component glyphs
+        // Local storage of storedTypefaces is formatted as keys = pageIds and values = Typeface objects
+        var storedTypefaces = yield figma.clientStorage.getAsync("typefaces");
+        console.log("typefaces:", storedTypefaces);
+        figma.showUI(__html__);
+        var names = [];
+        storedTypefaces.keys().forEach(function (id, index) {
+            names.push(storedTypefaces[id].name);
+        });
+        // default to first in list for now!
+        currTypeface = yield figma.clientStorage.getAsync("currTypeface");
+        figma.ui.postMessage({ typefaces: names });
+        // loadUI(storedTypefaces);
+    });
+}
+getTypefacesFromStorage();
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
     if (msg.type === 'log-components') {
-        logComponents();
+        logCurrentPage();
     }
     else if (msg.type === 'create-spacing') {
         createSpacingStrings();
@@ -21,21 +52,89 @@ figma.ui.onmessage = msg => {
         renderText(msg.data);
     }
     else if (msg.type == 'new-typeface') {
-        createNewTypeface();
+        createNewPage();
+    }
+    else if (msg.type == 'select-option') {
+        selectOption(msg.data);
     }
 };
-function logComponents() {
-    // all components must be glyphs in the short term
-    // figma.root.children.map(page => {
-    //page.findAll
-    figma.currentPage.findAll(n => n.type == 'COMPONENT').map(node => {
-        // map each node into a hash table with its name as it key for easy access
-        components[node.name] = node;
-    });
-    // })
-    // components = Object.keys(components).sort().reduce( 
-    //   (obj, key) => ({ obj[key]: components[key] }), {}
-    // );
+function selectOption(name) {
+    const selectedTypeface = typefacesByName[name];
+    selectedTypeface.setAsCurrent();
+}
+class Typeface {
+    constructor(id, name) {
+        this.glyphDict = {};
+        this.id = id;
+        this.name = name;
+        typefacesByName[name] = this;
+        currTypeface = this;
+        figma.ui.postMessage({ 'addOption': this.name });
+        // always log glyphs of new typefaces
+        this.logGlyphs();
+        this.updateLocalStorage();
+        this.setAsCurrent();
+    }
+    logGlyphs() {
+        // all components must be glyphs in the short term
+        // figma.root.children.map(page => {
+        //page.findAll
+        figma.currentPage.findAll(n => n.type == 'COMPONENT').map(node => {
+            // map each node into a hash table with its name as it key for easy access
+            this.glyphDict[node.name] = node;
+        });
+    }
+    updateLocalStorage() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // "typefaces" : {id: this}
+            // storedTypefaces evaluates to a dictionary where
+            // keys = pageIds and values = Typeface objects
+            // The Typeface object then contains everything we need for names and glyphsets
+            figma.clientStorage.getAsync("typefaces").then(storedTypefaces => {
+                if (storedTypefaces) {
+                    // The dict already exists
+                    // This typeface may already be in the dict, or not
+                    // In both cases, we just need to reassign the value
+                    storedTypefaces[this.id] = this;
+                    figma.clientStorage.setAsync("typefaces", storedTypefaces);
+                }
+                else {
+                    // The dict not exist yet. Need to create the dictionary
+                    var newDict = {};
+                    newDict[this.id] = this;
+                    figma.clientStorage.setAsync("typefaces", newDict);
+                }
+            });
+        });
+    }
+    // getGlyphsFromLocalStorage() {
+    //   return figma.clientStorage.getAsync(this.id);
+    // }
+    setAsCurrent() {
+        currTypeface = this;
+        console.log(this.name + "set as current");
+        figma.clientStorage.setAsync("currTypefaces", this);
+        // figma.ui.postMessage(this.name)
+    }
+}
+// function loadFromLocalStorage() {
+//   var typefaces = {};
+//   figma.root.children.map(page => {
+//     var t = await figma.clientStorage.getAsync(page.id);
+//   }
+//   return typefaces;
+// }
+function logCurrentPage() {
+    const currId = figma.currentPage.id;
+    var pageName = figma.currentPage.name;
+    if (pageName in typefacesByName) {
+        console.log("Log current page:", pageName + " is in typefacesByName");
+        const typeface = typefacesByName[pageName];
+        typeface.logGlyphs();
+    }
+    else {
+        typefacesByName[pageName] = new Typeface(currId, pageName);
+    }
 }
 let createFrame = function () {
     var frame = figma.createFrame();
@@ -63,8 +162,10 @@ function addGlyphsToFrame(string) {
         x = lastNode.x;
     }
     let frame = createFrame();
+    console.log("currTypeface", currTypeface.name);
     for (let glyph of string) {
         // if the current glyph exists in your component set
+        const components = currTypeface.glyphDict;
         if (components[glyph]) {
             frame.appendChild(components[glyph].createInstance());
         }
@@ -82,6 +183,7 @@ function addGlyphsToFrame(string) {
     return frame;
 }
 function createSpacingStrings() {
+    const components = currTypeface.glyphDict;
     // capitals only first
     // Create a a spacing string for every glyph component that exists within Capitals string
     for (const glyph of capitals) {
@@ -99,7 +201,7 @@ function renderText(string) {
     frame.name = string;
     figma.currentPage.appendChild(frame);
 }
-function createNewTypeface() {
+function createNewPage() {
     // The typeface will be created on a new page
     var page = figma.createPage();
     page.name = "New typeface";
--- code.ts
+++ code.ts
@@ -7,17 +7,45 @@ let numbers = "0123456789";
 
 let glyphSets = [capitals, lowercase, numbers];
 
-var components = {};
+var currTypeface;
+var currComponents = {};
 
 
-figma.showUI(__html__);
+// {name: Typeface}
+var typefacesByName = {};
 
+async function getTypefacesFromStorage() {
+  // What is necessary at startup
+  // List of names of typefaces
+  // All the Typeface class objects which contain component glyphs
+  // Local storage of storedTypefaces is formatted as keys = pageIds and values = Typeface objects
+  var storedTypefaces = await figma.clientStorage.getAsync("typefaces");
+
+  console.log("typefaces:", storedTypefaces);
+
+  figma.showUI(__html__);
+
+  var names = [];
+  storedTypefaces.keys().forEach(function (id, index) {
+    names.push(storedTypefaces[id].name);
+  }) 
+
+  // default to first in list for now!
+  currTypeface = await figma.clientStorage.getAsync("currTypeface");
+  
+  figma.ui.postMessage({typefaces: names})
+  // loadUI(storedTypefaces);
+}
+
+getTypefacesFromStorage();
+
+  
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = msg => {
   if (msg.type === 'log-components') {
-    logComponents();
+    logCurrentPage();
   } else if (msg.type === 'create-spacing') {
     createSpacingStrings();
     // figma.closePlugin();
@@ -25,30 +53,112 @@ figma.ui.onmessage = msg => {
   } else if (msg.type == 'add-text') {
     renderText(msg.data)
   } else if (msg.type == 'new-typeface') {
-    createNewTypeface();
+    createNewPage();
+  } else if (msg.type == 'select-option') {
+    selectOption(msg.data);
   }
 }
 
+function selectOption(name: string) {
+  const selectedTypeface = typefacesByName[name];
+  selectedTypeface.setAsCurrent();
+}
 
-function logComponents() {
-  // all components must be glyphs in the short term
-  // figma.root.children.map(page => {
-    //page.findAll
-    figma.currentPage.findAll(n => n.type == 'COMPONENT').map(node => {
-      // map each node into a hash table with its name as it key for easy access
-      components[node.name] = <ComponentNode> node;
+
+class Typeface {
+  id: string;
+  name: string;
+  glyphDict = {};
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+
+    typefacesByName[name] = this;
+
+    currTypeface = this;
+    figma.ui.postMessage({'addOption': this.name});
+
+    // always log glyphs of new typefaces
+    this.logGlyphs();
+    this.updateLocalStorage();
+    this.setAsCurrent();
+  }
+
+  logGlyphs() {
+    // all components must be glyphs in the short term
+    // figma.root.children.map(page => {
+      //page.findAll
+      figma.currentPage.findAll(n => n.type == 'COMPONENT').map(node => {
+        // map each node into a hash table with its name as it key for easy access
+        this.glyphDict[node.name] = <ComponentNode> node;
+      })
+  }
+
+
+  async updateLocalStorage() {
+    // "typefaces" : {id: this}
+    // storedTypefaces evaluates to a dictionary where
+    // keys = pageIds and values = Typeface objects
+    // The Typeface object then contains everything we need for names and glyphsets
+    figma.clientStorage.getAsync("typefaces").then(storedTypefaces => {
+      
+      if (storedTypefaces) {
+        // The dict already exists
+        // This typeface may already be in the dict, or not
+        // In both cases, we just need to reassign the value
+        storedTypefaces[this.id] = this;
+        figma.clientStorage.setAsync("typefaces", storedTypefaces);
+      } else {
+        // The dict not exist yet. Need to create the dictionary
+        var newDict = {};
+        newDict[this.id] = this;
+        figma.clientStorage.setAsync("typefaces", newDict);
+      }
     })
-  // })
-  // components = Object.keys(components).sort().reduce( 
-  //   (obj, key) => ({ obj[key]: components[key] }), {}
-  // );
+  }
+
+  // getGlyphsFromLocalStorage() {
+  //   return figma.clientStorage.getAsync(this.id);
+  // }
+
+  setAsCurrent() {
+    currTypeface = this;
+    console.log(this.name + "set as current")
+    figma.clientStorage.setAsync("currTypefaces", this);
+    
+    // figma.ui.postMessage(this.name)
+
+  }
+}
+
+// function loadFromLocalStorage() {
+//   var typefaces = {};
+//   figma.root.children.map(page => {
+//     var t = await figma.clientStorage.getAsync(page.id);
+//   }
+
+//   return typefaces;
+// }
+
+
+function logCurrentPage() {
+  const currId = figma.currentPage.id;
+  var pageName = figma.currentPage.name;
+  if (pageName in typefacesByName) {
+    console.log("Log current page:", pageName + " is in typefacesByName")
+    const typeface = typefacesByName[pageName]
+    typeface.logGlyphs();
+  } else {
+    typefacesByName[pageName] = new Typeface(currId, pageName);
+  }
 }
 
 let createFrame = function(): FrameNode {
   var frame = figma.createFrame();
-  
   return frame;
 }
+
 
 let buildSpacingString = function(glyph: string): string {
   var spacingString = "";
@@ -71,8 +181,11 @@ function addGlyphsToFrame(string: string) {
     x = lastNode.x;
   }
   let frame = createFrame();
+  console.log("currTypeface", currTypeface.name)
   for (let glyph of string) {
     // if the current glyph exists in your component set
+    
+    const components = currTypeface.glyphDict;
     if (components[glyph]) {
       frame.appendChild(components[glyph].createInstance());
     } else {
@@ -90,6 +203,8 @@ function addGlyphsToFrame(string: string) {
 }
 
 function createSpacingStrings() {
+  const components = currTypeface.glyphDict;
+
   // capitals only first
   // Create a a spacing string for every glyph component that exists within Capitals string
   for (const glyph of capitals) {
@@ -109,7 +224,7 @@ function renderText(string: string) {
   figma.currentPage.appendChild(frame);
 }
 
-function createNewTypeface() {
+function createNewPage() {
   // The typeface will be created on a new page
 
   var page = figma.createPage();
